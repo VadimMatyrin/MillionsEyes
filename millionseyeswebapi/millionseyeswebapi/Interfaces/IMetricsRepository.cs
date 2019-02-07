ea96d702d6f5b09b7052cd1d5cd8ebf3dca325e2
vadym.matyrin <vadym.matyrin@globallogic.com>
2019-02-07 12:01:23 +0200

    Add MetricModel type constraint to IMetricRepository

--- millionseyeswebapi/millionseyeswebapi/Interfaces/IMetricsRepository.cs
+++ millionseyeswebapi/millionseyeswebapi/Interfaces/IMetricsRepository.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using MillionsEyesWebApi.Models.MetricModels;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MillionsEyesWebApi.Models
 {
-    public interface IMetricsRepository<T>
+    public interface IMetricsRepository<T> where T : MetricModel
     {
         Task<IEnumerable<T>> GetMetricsAsync(int interval, DateTime startTime, DateTime endTime, string metricName = null);
     }
